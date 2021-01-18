fef4011e5ac3f27dfd31ee7e3a0623940d4a0f1b
Liam Moat <contact@liammoat.com>
2021-01-18 23:29:26 +0000

    deploy container web app

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "lm-devops-rg"
-    storage_account_name  = "lmdevopsstor"
-    container_name        = "tf-state"
-    key                   = "frontline.tfstate"
+    resource_group_name  = "lm-devops-rg"
+    storage_account_name = "lmdevopsstor"
+    container_name       = "tf-state"
+    key                  = "frontline.tfstate"
   }
 }
 
@@ -13,36 +13,76 @@ provider "azurerm" {
 }
 
 locals {
-    prefix = "frontline"
+  prefix = "frontline"
+
+  container_image = "${var.container_registry}/${var.container_image_name}:${var.container_image_tag}"
+
+  env_variables = {
+    DOCKER_REGISTRY_SERVER_URL      = "https://${var.container_registry}"
+    DOCKER_REGISTRY_SERVER_USERNAME = var.container_registry_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = var.container_registry_password
+  }
+
+  tags = {
+    owner = "terraform"
+  }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name = "${local.prefix}-rg-${var.env}"
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name     = "${local.prefix}-rg-${var.env}"
 }
 
 resource "azurerm_app_service_plan" "asp" {
-    name                = "${local.prefix}-asp-${var.env}"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    
-    sku {
-        tier = "Standard"
-        size = "S1"
-    }
+  name                = "${local.prefix}-asp-${var.env}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+  tags                = local.tags
+
+  sku {
+    tier     = "Standard"
+    size     = "S1"
+    capacity = "2"
+  }
 }
 
 resource "azurerm_app_service" "web" {
-    name                = "${local.prefix}-web-${var.env}"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.asp.id
+  name                = "${local.prefix}-web-${var.env}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+  tags                = local.tags
+
+  site_config {
+    always_on        = "true"
+    linux_fx_version = "DOCKER|${local.container_image}"
+  }
+
+  app_settings = merge(local.env_variables, {
+    AZURE_MONITOR_INSTRUMENTATION_KEY = azurerm_application_insights.insights.instrumentation_key
+  })
 }
 
 resource "azurerm_app_service_slot" "slot" {
-    name                = "${local.prefix}-web-${var.env}-slot"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.asp.id
-    app_service_name    = azurerm_app_service.web.name
+  name                = "${local.prefix}-web-${var.env}-slot"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_name    = azurerm_app_service.web.name
+  tags                = local.tags
+
+  site_config {
+    always_on = "true"
+  }
+
+  app_settings = local.env_variables
+}
+
+resource "azurerm_application_insights" "insights" {
+  name                = "${local.prefix}-appi-${var.env}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  application_type    = "web"
+  tags                = local.tags
 }
--- src/infra/variables.tf
+++ src/infra/variables.tf
@@ -3,7 +3,24 @@ variable "env" {
   default = "dev"
 }
 
-variable "location" {
+variable "container_registry" {
   type    = string
-  default = "UK South"
+}
+
+variable "container_registry_username" {
+  type    = string
+}
+
+variable "container_registry_password" {
+  type    = string
+  sensitive = true
+}
+
+variable "container_image_name" {
+  type    = string
+  default = "web"
+}
+
+variable "container_image_tag" {
+  type    = string
 }
