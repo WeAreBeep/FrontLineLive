811b1ec993e08fd84f77d20d436d8c467498e301
Liam Moat <contact@liammoat.com>
2021-01-19 09:06:56 +0000

    feat(sql-tf): initial strucuture for SQL resources

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -38,57 +38,10 @@ data "azurerm_container_registry" "acr" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = "${local.prefix}-asp-${var.env}"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
-  tags                = local.tags
-
-  sku {
-    tier     = "Standard"
-    size     = "S1"
-    capacity = "2"
-  }
-}
-
-resource "azurerm_app_service" "web" {
-  name                = "${local.prefix}-web-${var.env}"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-  tags                = local.tags
-
-  site_config {
-    always_on        = "true"
-    linux_fx_version = "DOCKER|${local.container_image}"
-  }
-
-  app_settings = merge(local.env_variables, {
-    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.insights.instrumentation_key
-  })
-}
-
-resource "azurerm_app_service_slot" "slot" {
-  name                = "${local.prefix}-web-${var.env}-slot"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-  app_service_name    = azurerm_app_service.web.name
-  tags                = local.tags
-
-  site_config {
-    always_on = "true"
-  }
-
-  app_settings = local.env_variables
-}
-
-resource "azurerm_application_insights" "insights" {
-  name                = "${local.prefix}-appi-${var.env}"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  application_type    = "web"
-  tags                = local.tags
+resource "azurerm_storage_account" "sa" {
+  name                     = "${local.prefix}sa${var.env}"
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
